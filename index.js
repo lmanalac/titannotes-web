7f6163029ae405d0ab90b4eb0ba6e3e4545717ef
lmanalac <lmanalac@mvesystems.com>
2018-07-01 17:09:30 -0700

    v0.2

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
 app.get('/', (req, res) => {
-  res.send('TitanNotes V0.01')
+  res.send('TitanNotes V0.02')
 })
 app.listen(3000, () => console.log('Server running on port 3000'))
